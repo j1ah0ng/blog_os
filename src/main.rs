0f64c6d3f6ba2c523b16bd5d0e64fe37461b12ca
Jiahong Long <jiahong888long@gmail.com>
2021-03-25 17:09:32 -0700

    Initial implementations of VGA framebuffer interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -13,14 +14,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (idx, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(idx as isize * 2) = byte;
-            *vga_buffer.offset(idx as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga_buffer::print_something();
     loop {}
 }

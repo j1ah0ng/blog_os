4d1bbe09a64b33ad6960bd78bc16f0fc7c53a8a7
Jiahong Long <jiahong888long@gmail.com>
2021-03-25 18:17:48 -0700

    Using lazily initialised statics and spinlocks to remove race condition in VGA interface

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@
 mod vga_buffer;
 use core::panic::PanicInfo;
 
+#[allow(dead_code)]
 static HELLO: &[u8] = b"Hello World!";
 
 /// Called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    panic!("oh no.");
     loop {}
 }
